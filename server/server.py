5c7ef2b206988309430ca568df9b3a6865bbf0ca
ByamB4 <byamba4life@gmail.com>
2022-02-15 21:05:03 +0800

    [web] full automated (flask + coinst) :rocket:

--- server/server.py
+++ server/server.py
@@ -1,35 +1,120 @@
 from flask import Flask, jsonify
-import os
-from apscheduler.schedulers.background import BackgroundScheduler
+import instaloader
 import atexit
-import time
 import tzlocal
+import os
+import boto3
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
 
 
-OBJECT_ENDPOINT = 'https://blabla.cdn.com'
-STATIC_ROOT = 'static'
-users = [
-    {
-        'name': 'themunkhjin',
-        'id': '10803123624',
-    },
-    {
-        'name': 'trader.erkhemee',
-        'id': '7417135025'
-    }
-]
+S3 = boto3.client('s3')
+BUCKET_NAME: str = 'coinst-story'
 
 
-def fetchStories():
+def getDirObjects(USERNAME, LISTED_OBJECTS) -> any:
+    return [object.get('Key')[len(f'{USERNAME}/'):] for object in LISTED_OBJECTS.get('Contents')][1:]
 
-    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
 
+class CoinstStory:
+    INST = None
+    TARGET_USERS = [
+        {
+            'name': 'themunkhjin',
+            'id': '10803123624',
+            'new_stories': 0
+        },
+        {
+            'name': 'trader.erkhemee',
+            'id': '7417135025',
+            'new_stories': 0
+        }
+    ]
+    STATIC_ROOT: str = 'static'
+    LOG: bool = True
 
-scheduler = BackgroundScheduler(daemon=True, timezone=tzlocal.get_localzone())
-scheduler.add_job(fetchStories, trigger="interval", seconds=10)
-scheduler.start()
+    def __init__(self):
+        self.start()
 
-atexit.register(lambda: scheduler.shutdown())
+    def start(self):
+        print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+        self.initInstLoader()
+
+        if self.LOG:
+            print('[+] Initialized values')
+
+        self.fetchStories()
+
+    def initInstLoader(self) -> None:
+        self.INST = instaloader.Instaloader()
+        self.INST.load_session_from_file('byamb4.bot')
+
+    def fetchStories(self) -> None:
+        os.chdir(self.STATIC_ROOT)
+        for stories in self.INST.get_stories([user.get('id') for user in self.TARGET_USERS]):
+            USERNAME: str = stories.owner_username
+            FILENAMES = getDirObjects(USERNAME, S3.list_objects(
+                Bucket=BUCKET_NAME, Prefix=f'{USERNAME}/'))
+            STORYIDS = self.getStoryIDS(FILENAMES)
+
+            if self.LOG:
+                print(f'[+] {USERNAME}')
+
+            NEW_STORY_COUNT: int = 0
+            for story in stories.get_items():
+                if not str(story.mediaid) in STORYIDS:
+                    NEW_STORY_COUNT += 1
+                    FILENAME: str = f'{story.date_local}|{story.mediaid}|{story.typename}'
+                    OUTNAME: str = str(story.date).replace(
+                        ' ', '_').replace(':', '-') + '_UTC'
+                    self.INST.download_storyitem(story, USERNAME)
+
+                    if story.typename == 'GraphStoryImage':
+                        os.rename(f'{USERNAME}/{OUTNAME}.jpg',
+                                  f'{USERNAME}/{FILENAME}.jpg')
+
+                        S3.upload_file(
+                            f'{USERNAME}/{FILENAME}.jpg',
+                            f'coinst-story',
+                            f'{USERNAME}/{FILENAME}.jpg'
+                        )
+                        os.remove(f'{USERNAME}/{FILENAME}.jpg')
+
+                    elif story.typename == 'GraphStoryVideo':
+                        os.rename(f'{USERNAME}/{OUTNAME}.mp4',
+                                  f'{USERNAME}/{FILENAME}.mp4')
+
+                        S3.upload_file(
+                            f'{USERNAME}/{FILENAME}.mp4',
+                            f'coinst-story',
+                            f'{USERNAME}/{FILENAME}.mp4'
+                        )
+                        os.remove(f'{USERNAME}/{FILENAME}.mp4')
+
+                        os.rename(f'{USERNAME}/{OUTNAME}.jpg',
+                                  f'{USERNAME}/{FILENAME}.jpg')
+
+                        S3.upload_file(
+                            f'{USERNAME}/{FILENAME}.jpg',
+                            f'coinst-story',
+                            f'{USERNAME}/{FILENAME}.jpg'
+                        )
+                        os.remove(f'{USERNAME}/{FILENAME}.jpg')
+
+                    os.remove(f'{USERNAME}/{OUTNAME}.json.xz')
+
+            targetUserIndex = [
+                user.get('name') for user in self.TARGET_USERS].index(USERNAME)
+            self.TARGET_USERS[targetUserIndex]['new_stories'] += NEW_STORY_COUNT
+            if self.LOG:
+                print(
+                    f'\t[+] New story count {self.TARGET_USERS[targetUserIndex]["new_stories"]}')
+
+        os.chdir('..')
+
+    def getStoryIDS(self, FILENAMES) -> any:
+        return [_.split('|')[1] for _ in FILENAMES]
+
 
 app = Flask(__name__)
 
@@ -44,35 +129,51 @@ def index() -> str:
 
 @app.route("/themunkhjin/stories", methods=["GET"])
 def munkhjinStories() -> str:
-    res = []
-    for file in os.listdir(f'{STATIC_ROOT}/themunkhjin'):
-        res.append({
+    USERNAME: str = 'themunkhjin'
+    RESULT = []
+    FILENAMES = getDirObjects(USERNAME, S3.list_objects(
+        Bucket=BUCKET_NAME, Prefix=f'{USERNAME}/'))
+
+    for file in FILENAMES:
+        RESULT.append({
             'date': file.split('|')[0],
             'type': file.endswith('mp4') and 'video' or 'image',
-            'url': f'{OBJECT_ENDPOINT}/{file}'
+            'url': f'https://coinst-story.s3.us-east-2.amazonaws.com/themunkhjin/{file}'
         })
 
     return jsonify({
         'status': 'ok',
-        'data': res,
+        'data': RESULT,
     })
 
 
 @app.route("/trader.erkhemee/stories", methods=["GET"])
 def erkhemeeStories() -> str:
-    res = []
-    for file in os.listdir(f'{STATIC_ROOT}/trader.erkhemee'):
-        res.append({
+    USERNAME: str = 'trader.erkhemee'
+    RESULT = []
+    FILENAMES = getDirObjects(USERNAME, S3.list_objects(
+        Bucket=BUCKET_NAME, Prefix=f'{USERNAME}/'))
+
+    for file in FILENAMES:
+        RESULT.append({
             'date': file.split('|')[0],
             'type': file.endswith('mp4') and 'video' or 'image',
-            'url': f'{OBJECT_ENDPOINT}/{file}'
+            'url': f'https://coinst-story.s3.us-east-2.amazonaws.com/trader.erkhemee/{file}'
         })
 
     return jsonify({
         'status': 'ok',
-        'data': res,
+        'data': RESULT,
     })
 
 
 if __name__ == '__main__':
+    coinst = CoinstStory()
+    scheduler = BackgroundScheduler(
+        daemon=True, timezone=tzlocal.get_localzone())
+    scheduler.add_job(coinst.start, 'interval', seconds=30)
+    scheduler.start()
+    atexit.register(lambda: scheduler.shutdown())
+
+    # Flask
     app.run(host='0.0.0.0', port=9001, debug=True, use_reloader=False)
